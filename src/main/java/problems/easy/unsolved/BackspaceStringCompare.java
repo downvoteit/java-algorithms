a194e4fd2a191c3b95731f09e4d0968d3c86ff52
downvoteit <tdlge897@gmail.com>
2022-04-03 02:42:52 +0500

    SquaresOfASortedArray, BackspaceStringCompare

--- src/main/java/problems/easy/unsolved/BackspaceStringCompare.java
+++
@@ -1,8 +0,0 @@
-package problems.easy.unsolved;
-
-// https://leetcode.com/problems/backspace-string-compare/
-public class BackspaceStringCompare {
-  public boolean backspaceCompare(String s, String t) {
-    return false;
-  }
-}
--- src/main/java/problems/easy/unsolved/SquaresOfASortedArray.java
+++
@@ -1,8 +0,0 @@
-package problems.easy.unsolved;
-
-// https://leetcode.com/problems/squares-of-a-sorted-array/
-public class SquaresOfASortedArray {
-  public int[] sortedSquares(int[] nums) {
-    return null;
-  }
-}
