4e92c069697f5ab6122833bbeb8a251e08196db9
downvoteit <tdlge897@gmail.com>
2022-04-03 20:42:33 +0500

    Refactor

--- src/test/java/problems/medium/depthfirstsearch/NumberOfIslandsTest.java
+++ src/test/java/problems/medium/depthfirstsearch/NumberOfIslandsTest.java
@@ -1,10 +1,9 @@
-package problems.medium.disjointsets;
+package problems.medium.depthfirstsearch;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-import problems.medium.depthfirstsearch.NumberOfIslands;
 
 import java.time.Duration;
 import java.util.stream.Stream;
